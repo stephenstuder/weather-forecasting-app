e3cee32be000f6eeebda75c27bf148c55865506f
Stephen Studer <stephenkstuder@gmail.com>
2020-04-04 22:22:51 -0700

    getting started

--- script.js
+++ script.js
@@ -3,16 +3,31 @@ $(document).ready(function(){
 
     const key = "69a8c21b7273a586293ec934e4d677e9";
     let location = "london,UK"
-    let url = "http://api.openweathermap.org/data/2.5/weather?q=" + location + "&APPID=" + key;
+    
+    //button listener
+    $("#search-button").on("click", function(){
+        location = $("#search-input").val();
+        let url = "http://api.openweathermap.org/data/2.5/weather?q=" + location +  "&APPID=" + key;
+        appendButton();
+        fillDashboard(url);
+    })
 
-    $.ajax({
-        url : url,
-        method : "GET"
-    }).then(function(response){
-        console.log(response);
+function appendButton(){
 
+}
 
-    });
+    //call API and build div
+   function fillDashboard(url){
+       $.ajax({
+           url : url,
+           method : "GET"
+       }).then(function(response){
+           console.log(response);
+   
+   
+       });
+       
+   }
 
 
 
