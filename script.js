d8357fbff54e26c3f1eef7b8879007b29c2b6788
stephen studer <stephenkstuder@gmail.com>
2020-04-05 20:55:41 -0700

    Weather Dashboard App

--- script.js
+++ script.js
@@ -1,32 +1,46 @@
 $(document).ready(function(){
-    console.log('it works');
 
     const key = "69a8c21b7273a586293ec934e4d677e9";
     let location = "london,UK"
     
-    //button listener
+    //Create and Search
     $("#search-button").on("click", function(){
         location = $("#search-input").val();
         let url = "http://api.openweathermap.org/data/2.5/forecast?q=" + location +  "&appid=" + key;
         //api.openweathermap.org/data/2.5/forecast?q={city name}&appid={your api key}
-        appendButton();
+        createButton(location);
         fillDashboard(url);
-    })
+    });
 
-function appendButton(){
+    //Search with Existing Button
+    $("#button-append").on("click", "button", function(){
+        let searchTerm = $(this).data("value");
+        console.log(searchTerm);
+        let url = "http://api.openweathermap.org/data/2.5/forecast?q=" + location +  "&appid=" + key;
+        fillDashboard(url);
+    });
 
-}
+    function createButton(location){
+        searchButton = $("<button>");
+        searchButton.addClass("search-button");
+        searchButton.attr("data-value", location);
+        searchButton.append(location);
+        $("#button-append").append(searchButton);
+    }
 
     //call API and build div
-   function fillDashboard(url){
-       $.ajax({
-           url : url,
-           method : "GET"
-       }).then(function(response){
-           console.log(response);
-            searchButton = $("<button>");
-            searchButton.append(response.city.name);
-            $("#button-append").append(searchButton);
+    function fillDashboard(url){
+        $.ajax({
+            url : url,
+            method : "GET"
+        }).then(function(response){
+            let cityName = response.city.name;
+            let currentTemp = response.list[0].main.temp;
+            console.log(response.list[0].main);
+            let cityNameH1 = $("<h1>")
+            cityNameH1.text("Current Weather for: " + cityName)
+            $("#append-weather-details").append(cityNameH1);
+            //Build the button in then 'then' because i dont want buttons if the search fails
    
        });
        
